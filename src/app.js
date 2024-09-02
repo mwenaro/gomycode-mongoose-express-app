13a14a5b61b7fee6e23fae278c8947781bdc7eec
Mwero Abdalla <mwerothewebmaker@gmail.com>
2024-09-02 20:41:28 +0300

    chore: update - replace mongoclient with mongoose

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const cors = require("cors");
-const dotenv = require('dotenv')
-const { MongoClient } = require("mongodb");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
 //load env variables
-dotenv.config()
+dotenv.config();
 
 //create app
 const app = express();
@@ -19,24 +19,48 @@ app.use(express.urlencoded({ extended: true }));
 // }));
 
 //connect mongodb here
-const url = process.env.MONGODB_URI
-let db;
-MongoClient.connect(url)
+const url = process.env.MONGODB_URI;
+mongoose
+  .connect(url)
   .then((client) => {
-    db = client.db("e-store");
     console.log("Mongodb conncted successfully");
   })
   .catch((error) => {
     console.log("An error has ocuured " + error.message);
   });
 
+//schema
+const UserSchema = new mongoose.Schema({
+  name: String,
+  age: Number,
+  isAdmin: Boolean,
+});
+
+//user Model
+const UserModel = mongoose.model("User", UserSchema);
+
 //routes
 app.get("/", async (_, res) => {
-  const data = await db.collection("products").find({}).toArray();
-  res.send(data);
-  console.log({ data });
+  //   const data = await db.collection("products").find({}).toArray();
+  //   res.send(data);
+  console.log({ data: "" });
 
   res.send({ message: "Hell from server" });
+});
+
+//user route
+app.get("/users", async (req, res) => {
+  const users = await UserModel.find({});
+  res.send(users);
+});
+//user route
+app.post("/users", async (req, res) => {
+  const newUser = new UserModel(req.body);
+  const savedUser = await newUser.save();
+
+  if (!savedUser) res.status(400).send({ message: " Invalid in put" });
+
+  res.status(201).send(savedUser);
 });
 
 app.all("*", (req, res) => res.status(404).send({ error: "Route Not Found" }));
